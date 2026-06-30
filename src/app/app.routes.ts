f3c38138eb764798ce801d914c6ba43ad4dabb2b
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-06-30 11:25:51 +0100

    Add angular routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Dashboard } from './pages/dashboard/dashboard';
+import { About } from './pages/about/about';
+
+
+export const routes: Routes=[
+
+
+{
+path:'',
+component:Dashboard
+},
+
+
+{
+path:'about',
+component:About
+}
+
+
+];
