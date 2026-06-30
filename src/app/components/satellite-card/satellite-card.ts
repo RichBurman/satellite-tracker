1918affc6822803bf1ea684f43ee33529a448159
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-06-30 10:25:24 +0100

    Create components

---
+++ src/app/components/satellite-card/satellite-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-satellite-card',
+  imports: [],
+  templateUrl: './satellite-card.html',
+  styleUrl: './satellite-card.css',
+})
+export class SatelliteCard {
+
+}
---
+++ src/app/components/satellite-list/satellite-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-satellite-list',
+  imports: [],
+  templateUrl: './satellite-list.html',
+  styleUrl: './satellite-list.css',
+})
+export class SatelliteList {
+
+}
